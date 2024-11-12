abe0c6ce29a90b7d16c7fa04dc020dd2291697b9
f-gehin-crm <FGehin@arfp.asso.fr>
2024-11-12 16:24:38 +0100

    fin de journée

---
+++ EXERCICESUITE/EXERCICESUITE/src/tableaulettre.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class tableaulettre{
+    
+
+
+/*  Écrire un algorithme qui déclare et remplit un tableau contenant les six voyelles de l’alphabet latin. */
+
+
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("\n" + " Hello, World !!!!"  + "\n");
+
+        Scanner sc = new Scanner(System.in); 
+
+        String [] tableau = new String[6];
+        int i = 0;
+
+
+for(i=0; i<=5; i ++) {
+
+        System.out.println("Entrez la voyelle " + (i+1) + "  :");
+            tableau [i]= sc.nextLine();
+
+                    }
+
+ for( i=0; i<=5; i++) {
+ System.out.print( tableau[i] +"  ");     
+ }
+ 
+
+
+
+    }
+
+}
