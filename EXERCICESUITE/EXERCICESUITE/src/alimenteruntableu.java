0de773f41989e78ea98189c143e9964e1a887361
f-gehin-crm <FGehin@arfp.asso.fr>
2024-11-13 09:11:59 +0100

    1er exercice du matin

---
+++ EXERCICESUITE/EXERCICESUITE/src/alimenteruntableu.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+
+
+
+public class alimenteruntableu {
+
+
+
+    public static void main(String[] args) throws Exception {
+    
+        int N = 0;
+        int pos = 0;
+        int neg = 0;
+        int zer = 0;
+        int i = 0;
+      
+        Scanner  sc = new Scanner( System.in);
+
+        System.out.println("Combien de valeurs voulez vous saisir ?  :  ");
+
+        N = sc.nextInt();
+
+        int TAB []= new int[N];
+
+        for( i=0; i<=(N-1); i++) 
+            {   System.out.println( " Entrez la " + (i+1)+ "ème valeur  :  ");
+
+                TAB[i] = sc.nextInt();
+
+                if ( TAB[i]>0) 
+                        { pos = pos +1; }
+                if (TAB[i]==0 ) 
+                         {zer = zer +1;}
+                if (TAB[i]<0) 
+                          {neg = neg +1;}
+
+
+            }
+
+                System.out.println(" il y a "+ pos + " valeurs positives.");
+                System.out.println( " il y a " + neg + " valeurs négatives");
+                System.out.println( " Et "+ zer + " valeurs nulles" + "\n");
+                System.out.println( " Voici le tableau  : " + "\n");
+
+                for (i=0; i<N ; i++)
+                {
+                    System.out.print( TAB[i]+ "  ");
+                }
+
+                System.out.println("\n"+"\n");
+
+                
+
+
+
+sc.close();
+System.exit(0);
+
+    
+
+}
+}
---
+++ EXERCICESUITE/EXERCICESUITE/src/entrainement.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+
+
+
+
+public interface entrainement {
+
+
+    public static void main( String[]  args) throws Exception {
+
+
+
+    }
+    
+}
