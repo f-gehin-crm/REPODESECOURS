5a70b399059dc901ee3bb9dc6ac14e356b070141
f-gehin-crm <FGehin@arfp.asso.fr>
2024-11-13 15:41:07 +0100

    fin de journée

---
+++ EXERCICESUITE/EXERCICESUITE/src/recherchedansuntableau.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+
+
+
+public class recherchedansuntableau {
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner (System.in);
+
+            System.out.println( "   HELLO WORLD    !!!!!");
+            int Nbentrées;
+
+            System.out.println( "Combien d'entrées allez vous effectuer  ?  : " );
+            Nbentrées= sc.nextInt();
+
+
+
+            /* Déclaration des variables */
+
+            int [] Tableau = new int [Nbentrées+1];
+            int i;
+            int Position;
+            int Valeursup;
+
+            /* Saisies des données */
+
+
+            for (i=0; i < Nbentrées; i++) 
+            {
+            System.out.println ("Entrez la " + (i+1) + "ème valeur  :  ");
+            Tableau[i]= sc.nextInt();
+            }
+
+            /* Traitement */
+            Position = 0;
+            Valeursup = 0;
+            Tableau[Nbentrées] = 0;
+            for ( i=0 ; i < Nbentrées; i ++ )
+            {
+                if ( Tableau[i] > Valeursup)
+                    {
+                        Valeursup = Tableau[i];
+                        Position = i;
+                    }
+            }
+
+            /* rendu des resultats */
+
+            System.out.println( Valeursup);
+            System.out.println( (Position + 1));
+
+
+        sc.close();
+        System.exit(0);
+
+    }
+    
+}
